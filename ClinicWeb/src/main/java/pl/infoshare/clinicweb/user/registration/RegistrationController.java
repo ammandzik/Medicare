788cc004939c9e28fe9990f5c17e71c8c11fa7a1
ammandzik <preisner.amanda@gmail.com>
2024-11-21 15:08:05 +0100

    removing unused import

--- ClinicWeb/src/main/java/pl/infoshare/clinicweb/user/registration/RegistrationController.java
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/user/registration/RegistrationController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import pl.infoshare.clinicweb.user.entity.Role;
 import pl.infoshare.clinicweb.user.service.UserService;
 
 @Controller

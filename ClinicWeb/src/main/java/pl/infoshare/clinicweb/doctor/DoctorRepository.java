03e4794532a549426b7effb3fa5b603107f3f449
ammandzik <preisner.amanda@gmail.com>
2024-11-22 18:38:08 +0100

    adding missing query in doctor repository, refactoring doctor service, adding session timeout

--- ClinicWeb/src/main/java/pl/infoshare/clinicweb/doctor/DoctorRepository.java
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/doctor/DoctorRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +12,9 @@ public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
     @Query("select d from Doctor d where d.details.pesel=:pesel")
     Optional<Doctor> findByPesel(String pesel);
+
+    @Query("select d from Doctor d where d.specialization=:specialization")
+    List<Doctor> findAllBySpecialization(Specialization specialization);
+
 
 }
